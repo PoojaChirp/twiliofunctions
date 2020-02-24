17e01f8926729d7588027b464d3657d624a69434
poojachirp <poojasrinath@gmail.com>
2020-02-24 14:34:14 -0500

    JS files

---
+++ getcredentials.js
@@ -0,0 +1,11 @@
+exports.handler = function(context, event, callback) {
+    const config = {
+        host: context.host,
+        port: context.port,
+        user: context.user,
+        password: context.password,
+        database: context.database
+    };
+    
+     callback(null,config);
+}
---
+++ mysql.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+const axios = require('axios');
+
+exports.handler = async function(context, event, callback) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    
+    try {
+        const config = await axios.get(`https://${context.DOMAIN_NAME}/getcredentials`);
+        const db = new Database(config.data);
+        
+        db.connection.connect();
+        const users = await db.query('select * from users');
+        await db.close();
+        callback(null, users);
+    } catch(e) {
+        callback(e);
+    }
+}
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
